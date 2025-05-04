b6aa783d181e63221ba13fc1d916a8997588f2d4
Alex Riley <ajcriley@gmail.com>
2025-05-04 21:47:47 +0100

    Add missing docstring for i. monad

--- src/jinx/execution/primitives.py
+++ src/jinx/execution/primitives.py
@@ -193,6 +193,7 @@ def dollar_dyad(x: np.ndarray, y: np.ndarray) -> np.ndarray:
 
 
 def idot_monad(y: np.ndarray) -> np.ndarray:
+    """i. monad: returns increasing/decreasing sequence of integer wrapperd to shape y."""
     arr = np.atleast_1d(y)
     shape = abs(arr)
     n = np.prod(shape)
